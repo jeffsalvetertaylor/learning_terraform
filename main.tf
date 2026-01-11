9d00dd63be8bda6ee6e9bd50ad0d6e3f6c1dc2be
jeffsalvetertaylor <jeff.salveter.taylor@gmail.com>
2026-01-10 20:00:12 -0500

    Fixed blog_alb name

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "{var.environment.name}-alb"
+  name = "${var.environment.name}-alb"
 
   load_balancer_type = "application"
 
